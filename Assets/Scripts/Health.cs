acd5d277c9c14cbb8825e36a204f10c897ba3bd4
Mai Huy Hoang <maihuyhoang13042002@gmail.com>
2022-06-10 21:19:11 +0700

    UI Slider - Unity Official Tutorials Link: https://www.youtube.com/watch?v=lNdhNHNwpjI&list=PLX2vGYjWbI0Qp0sD8_RKgbWul7z_eyNAv&index=7

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Slider slider;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = Mathf.MoveTowards(slider.value, 100.0f, 0.15f);
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+
+    public void AdjustSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+}
