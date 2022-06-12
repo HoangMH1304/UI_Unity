8136000525b376d89933f9443264d93370676efe
Mai Huy Hoang <maihuyhoang13042002@gmail.com>
2022-06-12 15:42:45 +0700

    UI Scroll Rect - Unity Official Tutorials Link: https://www.youtube.com/watch?v=tcU8yzv_xEw&list=PLX2vGYjWbI0Qp0sD8_RKgbWul7z_eyNAv&index=9

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     public float speed = 10f;
-    // Update is called once per frame
+    
     void Update()
     {
         transform.Rotate(0, speed * Time.deltaTime, 0);
